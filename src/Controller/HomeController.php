75045c38fe65d07c9d1b4d1d30c6b7946e1c0f35
Djessa <s.djessa.2000@gmail.com>
2021-02-26 21:54:40 +0100

    Listing de produit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 use App\Repository\ProductRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(ProductRepository $products): Response
+    public function index(ProductRepository $productRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        $products = $paginator->paginate(
+            $productRepository->findBy([], ['id' => 'DESC']),
+            $request->query->getInt('page', 1),
+            12
+        );
         return $this->render('home/index.html.twig', compact('products'));
     }
 }
