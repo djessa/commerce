219c89f1746ab8ccc1a62ea6d8d3a35a68de111f
Djessa <s.djessa.2000@gmail.com>
2021-02-26 15:00:40 +0100

    Refacotring

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Import styles 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '../css/app.css';
+
+// Import scripts
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+
